d9c8911de76dda40ab0920a2bbfeb91a09e1acf4
TommasoPieroncini <tommipier96@hotmail.it>
2017-01-20 18:26:26 -0500

    fixes

--- layout/graphs/graph.js
+++ layout/graphs/graph.js
@@ -3,10 +3,8 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
   ScrollView,
-  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 
@@ -30,15 +28,17 @@ export default class GraphPage extends Component {
       activeIndex: 0,
       spendingsPerYear: data.spendingsPerYear,
     };
-    this._onPieItemSelected = this._onPieItemSelected.bind(this);
-    this._shuffle = this._shuffle.bind(this);
+    this.onPieItemSelected = this.onPieItemSelected.bind(this);
+    this.shuffle = this.shuffle.bind(this);
   }
 
-  _onPieItemSelected(newIndex){
-    this.setState({...this.state, activeIndex: newIndex, spendingsPerYear: this._shuffle(data.spendingsPerYear)});
+  onPieItemSelected(newIndex){
+    this.setState({ ...this.state,
+        activeIndex: newIndex,
+        spendingsPerYear: this.shuffle(data.spendingsPerYear) });
   }
 
-  _shuffle(a) {
+  shuffle(a) {
       for (let i = a.length; i; i--) {
           let j = Math.floor(Math.random() * i);
           [a[i - 1], a[j]] = [a[j], a[i - 1]];
@@ -53,21 +53,25 @@ export default class GraphPage extends Component {
     return (
       <ScrollView>
         <View style={styles.container} >
-          <Text style={styles.chart_title}>Distribution of spending this month</Text>
+          <Text style={styles.charttitle}>Distribution of spending this month</Text>
           <Pie
             pieWidth={150}
             pieHeight={150}
-            onItemSelected={this._onPieItemSelected}
+            onItemSelected={this.onPieItemSelected}
             colors={Theme.colors}
             width={width}
             height={height}
-            data={data.spendingsLastMonth} />
-          <Text style={styles.chart_title}>Spending per year in {data.spendingsLastMonth[this.state.activeIndex].name}</Text>
+            data={data.spendingsLastMonth}
+          />
+          <Text style={styles.charttitle}>
+          Spending per year in {data.spendingsLastMonth[this.state.activeIndex].name}
+          </Text>
           <AreaSpline
             width={width}
             height={height}
             data={this.state.spendingsPerYear}
-            color={Theme.colors[this.state.activeIndex]} />
+            color={Theme.colors[this.state.activeIndex]}
+          />
         </View>
       </ScrollView>
     );
@@ -76,17 +80,17 @@ export default class GraphPage extends Component {
 
 const styles = {
   container: {
-    backgroundColor:'whitesmoke',
+    backgroundColor: 'whitesmoke',
     marginTop: 21,
   },
-  chart_title : {
+  chart_title: {
     paddingTop: 15,
     textAlign: 'center',
     paddingBottom: 5,
     paddingLeft: 5,
     fontSize: 18,
-    backgroundColor:'white',
+    backgroundColor: 'white',
     color: 'grey',
-    fontWeight:'bold',
+    fontWeight: 'bold',
   }
-}
+};
