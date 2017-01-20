eaede9fb10d7968fa9f5d4486586efff32fb87fb
TommasoPieroncini <tommipier96@hotmail.it>
2017-01-20 18:23:56 -0500

    Tommy exploring graph apis

--- layout/graphs/graph.js
+++ layout/graphs/graph.js
@@ -1,92 +1,38 @@
-// @flow
-'use strict';
-
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Chart from 'react-native-chart';
 
-import AreaSpline from './../../js/charts/AreaSpline';
-import Pie from './../../js/charts/Pie';
-import Theme from './../../js/theme';
-import data from './../../resources/data';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    chart: {
+        width: 200,
+        height: 200,
+    },
+});
 
-type State = {
-  activeIndex: number,
-  spendingsPerYear: any
-}
+const data = [[
+    [0, 1],
+    [1, 3],
+    [3, 7],
+    [4, 9],
+]];
 
 export default class GraphPage extends Component {
-
-  state: State;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeIndex: 0,
-      spendingsPerYear: data.spendingsPerYear,
-    };
-    this._onPieItemSelected = this._onPieItemSelected.bind(this);
-    this._shuffle = this._shuffle.bind(this);
-  }
-
-  _onPieItemSelected(newIndex){
-    this.setState({...this.state, activeIndex: newIndex, spendingsPerYear: this._shuffle(data.spendingsPerYear)});
-  }
-
-  _shuffle(a) {
-      for (let i = a.length; i; i--) {
-          let j = Math.floor(Math.random() * i);
-          [a[i - 1], a[j]] = [a[j], a[i - 1]];
-      }
-      return a;
-  }
-
-  render() {
-    const height = 200;
-    const width = 500;
-
-    return (
-      <ScrollView>
-        <View style={styles.container} >
-          <Text style={styles.chart_title}>Distribution of spending this month</Text>
-          <Pie
-            pieWidth={150}
-            pieHeight={150}
-            onItemSelected={this._onPieItemSelected}
-            colors={Theme.colors}
-            width={width}
-            height={height}
-            data={data.spendingsLastMonth} />
-          <Text style={styles.chart_title}>Spending per year in {data.spendingsLastMonth[this.state.activeIndex].name}</Text>
-          <AreaSpline
-            width={width}
-            height={height}
-            data={this.state.spendingsPerYear}
-            color={Theme.colors[this.state.activeIndex]} />
-        </View>
-      </ScrollView>
-    );
-  }
-}
-
-const styles = {
-  container: {
-    backgroundColor:'whitesmoke',
-    marginTop: 21,
-  },
-  chart_title : {
-    paddingTop: 15,
-    textAlign: 'center',
-    paddingBottom: 5,
-    paddingLeft: 5,
-    fontSize: 18,
-    backgroundColor:'white',
-    color: 'grey',
-    fontWeight:'bold',
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Chart
+                    style={styles.chart}
+                    data={data}
+                    verticalGridStep={5}
+                    showDataPoint={true}
+                />
+            </View>
+        );
+    }
 }
