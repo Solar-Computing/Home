b629604a0364dd13d8825c52c981583fc23e4bfe
TommasoPieroncini <tommipier96@hotmail.it>
2017-01-22 13:43:19 -0500

    fixed hardcoded data

--- layout/settings/settingsPage.js
+++ layout/settings/settingsPage.js
@@ -17,26 +17,26 @@ let mySwitch: SubSwitch;
 const allSwitches = [];
 
 // myData = [
-//   {"name": "General", "state": {"switchIsOn": false}, "options": [
+//   {"name": "General", "toggle": {"switchIsOn": false}, "options": [
 //     {"name": "Temperature", "optionType": "slider", "state": {"minimum": "20", "maximum":"120"}},
 //     {"name": "Microwave", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Dishwasher", "optionType": "switch", "state": {"switchIsOn": false}}
 //     ]
 //   },
-//   {"name": "Kitchen", "state": {"switchIsOn": false}, "options": [
+//   {"name": "Kitchen", "toggle": {"switchIsOn": false}, "options": [
 //     {"name": "Lights", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Microwave", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Dishwasher", "optionType": "switch", "state": {"switchIsOn": false}}
 //     ]
 //   },
-//   {"name": "Living Room", "state": {"switchIsOn": false}, "options": [
+//   {"name": "Living Room", "toggle": {"switchIsOn": false}, "options": [
 //     {"name": "Light 1", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Light 2", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Outlet 1", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Outlet 2", "optionType": "switch", "state": {"switchIsOn": false}}
 //     ]
 //   },
-//   {"name": "Bed Room", "state": {"switchIsOn": false}, "options": [
+//   {"name": "Bed Room", "toggle": {"switchIsOn": false}, "options": [
 //     {"name": "Outlet 1", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Outlet 2", "optionType": "switch", "state": {"switchIsOn": false}},
 //     {"name": "Heater", "optionType": "switch", "state": {"switchIsOn": false}},
@@ -50,6 +50,7 @@ const allSwitches = [];
 //     ]
 //   }
 // ]
+
 
 
 class SettingsPage extends Component {
