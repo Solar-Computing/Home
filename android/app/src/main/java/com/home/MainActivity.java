1e29b7c2a8328aaae673476a56b863110f432a72
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-03-12 13:45:06 -0400

    graph changes

--- android/app/src/main/java/com/home/MainActivity.java
+++ android/app/src/main/java/com/home/MainActivity.java
@@ -1,7 +1,6 @@
 package com.home;
 
 import com.facebook.react.ReactActivity;
-import com.devfd.RNGeocoder.RNGeocoderPackage;
 
 public class MainActivity extends ReactActivity {
 
--- android/app/src/main/java/com/home/MainApplication.java
+++ android/app/src/main/java/com/home/MainApplication.java
@@ -1,16 +1,12 @@
 package com.home;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.devfd.RNGeocoder.RNGeocoderPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RNGeocoderPackage()
+          new MainReactPackage()
       );
     }
   };
@@ -37,9 +32,9 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
-//  @Override
-//  public void onCreate() {
-//    super.onCreate();
-//    SoLoader.init(this, /* native exopackage */ false);
-//  }
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
