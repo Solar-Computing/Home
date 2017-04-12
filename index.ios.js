c01b61b7e351dce716b3bff52dbff1e585ee3fd9
Aayush Kumar <aayush.kumarmail@gmail.com>
2017-04-12 10:42:06 -0400

    Removed extraneous imports and components from index files

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  View
 } from 'react-native';
-import App from './app.js';
 import Login from './layout/login/login.js';
 
 class Home extends Component {
   render() {
     return (
-      <View>
-        <Login />
-      </View>
+      <Login />
     );
   }
 }
 
-AppRegistry.registerComponent('Home', () => Login);
+AppRegistry.registerComponent('Home', () => Home);
