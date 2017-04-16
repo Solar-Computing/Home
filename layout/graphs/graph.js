c2d3f0789c9f95e9de426cd76f5401b4b9cbb522
Aayush Kumar <aayush.kumarmail@gmail.com>
2017-04-16 15:34:05 -0400

    resolved first round of errors

--- layout/graphs/graph.js
+++ layout/graphs/graph.js
@@ -346,247 +346,53 @@ export default class GraphPage extends Component {
     });
   }
 
+
   render() {
-    /*const profitData = [
-      [{
-        x: 0,
-        y: -5
-      }, {
-        x: 2,
-        y: -20
-      }, {
-        x: 4,
-        y: -25
-      }, {
-        x: 6,
-        y: -20
-      }, {
-        x: 8,
-        y: -10
-      }, {
-        x: 10,
-        y: -5
-      }, {
-        x: 12,
-        y: -10
-      }, {
-        x: 14,
-        y: 20
-      }, {
-        x: 16,
-        y: 15
-      }, {
-        x: 18,
-        y: 10
-      }, {
-        x: 20,
-        y: -8
-      }, {
-        x: 22,
-        y: -15
-      }, {
-        x: 24,
-        y: -5
-      }]
-    ];
-
-    const waterData = [
-      [{
-        x: 0,
-        y: 10
-      }, {
-        x: 2,
-        y: 10
-      }, {
-        x: 4,
-        y: 12
-      }, {
-        x: 6,
-        y: 10
-      }, {
-        x: 8,
-        y: 15
-      }, {
-        x: 10,
-        y: 40
-      }, {
-        x: 12,
-        y: 35
-      }, {
-        x: 14,
-        y: 25
-      }, {
-        x: 16,
-        y: 10
-      }, {
-        x: 18,
-        y: 8
-      }, {
-        x: 20,
-        y: 36
-      }, {
-        x: 22,
-        y: 15
-      }, {
-        x: 24,
-        y: 5
-      }],
-      [{
-        x: 0,
-        y: 4
-      }, {
-        x: 2,
-        y: 4
-      }, {
-        x: 4,
-        y: 5
-      }, {
-        x: 6,
-        y: 4
-      }, {
-        x: 8,
-        y: 6
-      }, {
-        x: 10,
-        y: 26
-      }, {
-        x: 12,
-        y: 19
-      }, {
-        x: 14,
-        y: 10
-      }, {
-        x: 16,
-        y: 6
-      }, {
-        x: 18,
-        y: 5
-      }, {
-        x: 20,
-        y: 20
-      }, {
-        x: 22,
-        y: 8
-      }, {
-        x: 24,
-        y: 2
-      }]
-    ];
-
-    const profitOptions = {
-      width: 300,
-      height: 150,
-      color: '#f2f2f2',
-      margin: {
-        top: 40,
-        left: 40,
-        bottom: 40,
-        right: 40
-      },
-      animate: {
-        type: 'delayed',
-        duration: 200
-      },
-      axisX: {
-        showAxis: true,
-        showLines: false,
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'bottom',
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 14,
-          fontWeight: true,
-          fill: '#f2f2f2'
+    const graphOptions = {
+        width: 300,
+        height: 150,
+        color: '#f2f2f2',
+        margin: {
+          top: 40,
+          left: 40,
+          bottom: 40,
+          right: 40
+        },
+        animate: {
+          type: 'delayed',
+          duration: 200
+        },
+        axisX: {
+          showAxis: true,
+          showLines: false,
+          showLabels: true,
+          showTicks: true,
+          zeroAxis: false,
+          orient: 'bottom',
+          label: {
+            fontFamily: 'Arial',
+            fontSize: 14,
+            fontWeight: true,
+            fill: '#f2f2f2'
+          }
+        },
+        axisY: {
+          showAxis: true,
+          showLines: true,
+          // color: '#4e5865',
+          showLabels: true,
+          showTicks: true,
+          zeroAxis: false,
+          orient: 'left',
+          label: {
+            fontFamily: 'Arial',
+            fontSize: 13,
+            fontWeight: true,
+            fill: '#f2f2f2'
+          }
         }
-      },
-      axisY: {
-        showAxis: true,
-        showLines: true,
-        // color: '#4e5865',
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'left',
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 13,
-          fontWeight: true,
-          fill: '#f2f2f2'
-        }
-      }
     };
-
-    /*
-    const waterOptions = {
-      width: 300,
-      height: 150,
-      color: '#59bbda',
-      margin: {
-        top: 40,
-        left: 40,
-        bottom: 40,
-        right: 40
-      },
-      animate: {
-        type: 'delayed',
-        duration: 200
-      },
-      axisX: {
-        showAxis: true,
-        showLines: false,
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'bottom',
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 14,
-          fontWeight: true,
-          fill: '#34495E'
-        }
-      },
-      axisY: {
-        showAxis: true,
-        showLines: true,
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'left',
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 14,
-          fontWeight: true,
-          fill: '#34495E'
-        }
-      }
-    };
-
-    return (
-      <ScrollView>
-        <Chart
-          title={'Cash Flow'}
-          units={'Dollars'}
-          data={profitData}
-          options={profitOptions}
-        />
-        <View style={styles.divider} />
-        <Chart
-          title={'Energy Consumption vs Production'}
-          units={'kW/h'}
-          data={energyData}
-          options={energyOptions}
-        />
-        <View style={styles.divider} />
-        <Chart
-          title={'Water Use vs Filtration'}
-          units={'Gal'}
-          data={waterData}
-          options={waterOptions}
-        />        
-      </ScrollView>
-    );*/
+    
     x = this.state.day.toLocaleString().split(' ');
     dayTitle = `${x[0]  } ${  x[1]  } ${  x[2]  }, ${  x[4]}`;
     return (
@@ -596,7 +402,7 @@ export default class GraphPage extends Component {
           units={'kWh'}
           day={dayTitle}
           data={dayPowerData}
-          options={energyOptions}
+          options={graphOptions}
           update={(newDay) => { this.update(newDay); }}
           today={new Date()}
           loaded={this.state.loaded}
