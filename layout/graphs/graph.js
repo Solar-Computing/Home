a1db0d66266330798dd03494bcf78401d1a55fda
TommasoPieroncini <tommipier96@hotmail.it>
2017-04-17 23:26:25 -0400

    tommy is working for eventual presentation

--- layout/graphs/graph.js
+++ layout/graphs/graph.js
@@ -275,6 +275,7 @@ export default class GraphPage extends Component {
 
   componentDidMount() {
     // Get correct dates
+    this.state.day.setHours(this.state.day.getHours() - 1)
     this.update(this.state.day)
   }
 
